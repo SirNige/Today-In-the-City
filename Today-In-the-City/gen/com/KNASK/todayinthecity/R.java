397b65688c9ae0455256f69f23fc6eb1fae714d5
KenedyD <KenedyD@hotmail.com>
2014-10-06 16:25:49 -0400

    10/6/2014 Commit
    Added activities for main menu, searching and creating an event.

--- Today-In-the-City/gen/com/KNASK/todayinthecity/R.java
+++ Today-In-the-City/gen/com/KNASK/todayinthecity/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -24,18 +29,43 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_create=0x7f080011;
+        public static final int action_search=0x7f080012;
+        public static final int action_search2=0x7f080013;
+        public static final int action_settings=0x7f080010;
+        public static final int button1=0x7f08000d;
+        public static final int button2=0x7f08000e;
+        public static final int button3=0x7f08000c;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080003;
+        public static final int editText3=0x7f080005;
+        public static final int editText4=0x7f080007;
+        public static final int editText5=0x7f080009;
+        public static final int expandableListView1=0x7f08000a;
+        public static final int linearLayout1=0x7f08000b;
+        public static final int listView1=0x7f08000f;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080006;
+        public static final int textView5=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_create=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_search=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int create=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int search=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_create=0x7f050004;
+        public static final int title_activity_search=0x7f050003;
     }
     public static final class style {
         /** 
--- Today-In-the-City/src/com/KNASK/todayinthecity/MainActivity.java
+++ Today-In-the-City/src/com/KNASK/todayinthecity/MainActivity.java
@@ -3,6 +3,10 @@ package com.KNASK.todayinthecity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +18,28 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            openSearch();
+	            return true;
+	        case R.id.action_search2:
+	        	openSearch();
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+	private void openSearch() {
+	    Toast.makeText(this, "Search button pressed", Toast.LENGTH_SHORT).show();
+	}
+
+
+	
 }
