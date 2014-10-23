bc7a4c2b77872c0bffb26bc0aac79ed405b89235
sangdonl <sdlee71@gmail.com>
2014-10-23 17:57:44 -0400

    Added calendar and timer picker dialog in CreateShow Activity and Added some Icons.

--- Today-In-the-City/src/com/KNASK/todayinthecity/CreateActivity.java
+++ Today-In-the-City/src/com/KNASK/todayinthecity/CreateActivity.java
@@ -1,28 +1,55 @@
 package com.KNASK.todayinthecity;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import com.KNASK.todayinthecitymodel.ShowEvent;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class CreateActivity extends Activity {
+public class CreateActivity extends Activity implements OnClickListener {
 
-	//ShowEvent showEvents = (ShowEvent) getApplicationContext();
-	 
+	ShowEvent showEvents;
+	
+	//widget GUI
+	ImageButton btnCalendar, btnTimePicker;
+	EditText 	txtDate, txtTime;
+	
+	// Variable for storing current date and time
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create);
 		
+		showEvents = (ShowEvent) getApplicationContext();
+		
+		// put Date Picker Dialog box  and Time Picker Dialog box code in Activity class
+        btnCalendar = (ImageButton) findViewById(R.id.imageCalendar);
+        btnTimePicker = (ImageButton) findViewById(R.id.imageTime);
+ 
+        txtDate = (EditText) findViewById(R.id.editDate);
+        txtTime = (EditText) findViewById(R.id.editTime);
+ 
+        btnCalendar.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
+        
 		
 		//this list must come from a band table of database.
 		Spinner spinner = (Spinner) findViewById(R.id.spinnerBand);
@@ -37,6 +64,54 @@ public class CreateActivity extends Activity {
 		
 	}
 
+    @Override
+    public void onClick(View v) {
+ 
+        if (v == btnCalendar) {
+ 
+            // Process to get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+ 
+            // Launch Date Picker Dialog
+            DatePickerDialog dpd = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+ 
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                int monthOfYear, int dayOfMonth) {
+                            // Display Selected date in textbox
+                            txtDate.setText((monthOfYear + 1) + "/"
+                                    + dayOfMonth + "/" + year);
+ 
+                        }
+                    }, mYear, mMonth, mDay);
+            dpd.show();
+        }
+        if (v == btnTimePicker) {
+ 
+            // Process to get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+ 
+            // Launch Time Picker Dialog
+            TimePickerDialog tpd = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+ 
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                int minute) {
+                            // Display Selected time in textbox
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            tpd.show();
+        }
+    }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
